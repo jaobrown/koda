56ebc5029cf098e7fbecbeabd2237e59bbe5bfe1
Jared Brown <jaobrown@iu.edu>
2019-06-21 21:13:32 -0400

    Add Links to nav component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { window } from "browser-monads"
-// import logo from "../../images/compass-logo.svg"
+import logo from "../../images/logo.png"
 // import "./nav.css"
 
 const Nav = () => (
@@ -12,13 +12,44 @@ const Nav = () => (
       </a>
       <Link
         className={
-          window.location.href.indexOf("contact") > 0
+          window.location.href.indexOf("about") > 0
             ? "nav__item--link active"
             : "nav__item--link"
         }
-        to="/contact"
+        to="/about"
       >
-        Contact
+        about
+      </Link>
+      <Link
+        className={
+          window.location.href.indexOf("work") > 0 ||
+          window.location.href.indexOf("category") > 0
+            ? "nav__item--link active"
+            : "nav__item--link"
+        }
+        to="/portfolio"
+      >
+        portfolio
+      </Link>
+      <Link
+        className={
+          window.location.href.indexOf("services") > 0
+            ? "nav__item--link active"
+            : "nav__item--link"
+        }
+        to="/services"
+      >
+        services
+      </Link>
+      <Link
+        className={
+          window.location.href.indexOf("process") > 0
+            ? "nav__item--link active"
+            : "nav__item--link"
+        }
+        to="/process"
+      >
+        process
       </Link>
       <Link
         className={
@@ -29,7 +60,17 @@ const Nav = () => (
         }
         to="/blog"
       >
-        Blog
+        blog
+      </Link>
+      <Link
+        className={
+          window.location.href.indexOf("contact") > 0
+            ? "nav__item--link active"
+            : "nav__item--link"
+        }
+        to="/contact"
+      >
+        contact
       </Link>
     </div>
   </nav>

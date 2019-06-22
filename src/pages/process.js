25915b64838d5b5d54dfb1f54fc2a0b5d7cc0756
Jared Brown <jaobrown@iu.edu>
2019-06-21 21:04:57 -0400

    Add pages

---
+++ src/pages/process.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ProcessPage = () => (
+  <Layout>
+    <SEO title="Process" />
+    <h1>Process</h1>
+  </Layout>
+)
+
+export default ProcessPage

aa79f4092219c414cd3d0d85c024f312e545a54f
Jared Brown <jaobrown@iu.edu>
2019-06-25 23:25:21 -0400

    Add Colors to StyleGuide

--- src/pages/style-guide.js
+++ src/pages/style-guide.js
@@ -2,8 +2,13 @@ import React from "react"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../utils/Seo"
-import { StyleGuideWrapper, StyledGuideHr } from "../styles/StyleGuideStyles"
-import { colors as ThemeColors} from "../styles/GlobalStyles"
+import {
+  StyleGuideWrapper,
+  StyledGuideHr,
+  ColorDivRow,
+  ColorDiv,
+} from "../styles/StyleGuideStyles"
+import { colors as ThemeColors } from "../styles/GlobalStyles"
 import { Text, Heading1, Heading2, Heading3 } from "../styles/Typography"
 
 const StyleGuidePage = () => (
@@ -12,18 +17,42 @@ const StyleGuidePage = () => (
     <StyleGuideWrapper>
       <Heading1>STYLE GUIDE</Heading1>
       <StyledGuideHr />
-      <Heading3 color={ThemeColors.orange}>Typography Baseline</Heading3>
-      <Heading1>1. Heading One</Heading1>
-      <Heading2>2. Heading Two</Heading2>
-      <Heading3>3. View Our Services</Heading3>
+      <Heading3 color={ThemeColors.orange}>Type Core</Heading3>
+      <Heading1 style={{ width: "520px" }}>
+        h1 – Heading One
+      </Heading1>
+      <Heading2>h2 – Heading Two</Heading2>
+      <Heading3>h3 Heading Three</Heading3>
       <Text>P – Body Copy</Text>
-      <Text style={{ width: "30%" }}>
+      <Text style={{ width: "520px" }}>
         A little bit about me as a designer, what I’m known for. A little bit
         about me as a designer, what I’m known for. A little bit about me as a
         designer, what I’m known, A little bit about me as a designer, what I’m
         known for. A little bit about me as a designer, what I’m known for
       </Text>
       <StyledGuideHr />
+      <Heading3 color={ThemeColors.orange}>Colors</Heading3>
+      <Text>Colorful Colors</Text>
+      <ColorDivRow>
+        <ColorDiv bg={ThemeColors.orange} />
+        <ColorDiv bg={ThemeColors.red} />
+        <ColorDiv bg={ThemeColors.teal} />
+        <ColorDiv bg={ThemeColors.blue} />
+      </ColorDivRow>
+      <Text>Flat Colors</Text>
+      <ColorDivRow>
+        <ColorDiv bg={ThemeColors.lightgreen} />
+        <ColorDiv bg={ThemeColors.darkgreen} />
+        <ColorDiv bg={ThemeColors.lightgreen} />
+        <ColorDiv bg={ThemeColors.tan} />
+        <ColorDiv bg={ThemeColors.lighttan} />
+        <ColorDiv bg={ThemeColors.coral} />
+      </ColorDivRow>
+      <Text>Utility Colors</Text>
+      <ColorDivRow>
+        <ColorDiv bg={ThemeColors.darkgray} />
+        <ColorDiv style={{border: `1px solid ${ThemeColors.darkgray}`}}bg={ThemeColors.alexwhite} />
+      </ColorDivRow>
     </StyleGuideWrapper>
   </Layout>
 )

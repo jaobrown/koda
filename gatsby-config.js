0ce229a48a147ef32261bfd3799f34c8f1b00bb3
Jared Brown <jared.brown@elementthree.com>
2020-03-16 20:48:47 -0400

    Trying netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     author: `@jaobrown`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-styled-components`,
